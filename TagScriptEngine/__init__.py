e6ee704a5eb575e3285e8cb6f3e8ad3ad7e84cb8
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2023-10-16 14:47:36 +0530

    Update __init__.py

--- TagScriptEngine/__init__.py
+++ TagScriptEngine/__init__.py
@@ -8,7 +8,7 @@ from .interpreter import *
 from .utils import *
 from .verb import Verb
 
-__version__ = "3.0.2"
+__version__ = "3.0.3"
 
 
 class VersionInfo(namedtuple("VersionInfo", "major minor micro")):
